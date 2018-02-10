00d410f16b6cb67cbe3ad302a8dc5ad8a47150fe
silvereyes <silvereyes333@gmail.com>
2018-02-09 18:04:07 -0600

    Version 14
    - API bump for Update 17 Dragon Bones

--- Roomba.lua
+++ Roomba.lua
@@ -759,7 +759,7 @@ local function InitialiseSettings()
 		name = ADDON_NAME,
 		displayName = ZO_HIGHLIGHT_TEXT:Colorize(ADDON_NAME),
 		author = "Wobin, CrazyDutchGuy & Ayantir",
-		version = "13",
+		version = "14",
 		slashCommand = "/roomba",
 		registerForRefresh = true,
 		registerForDefaults = true,
