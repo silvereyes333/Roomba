6a216481a46d1f96c4609d1eaf68785408421085
silvereyes <silvereyes333@gmail.com>
2019-01-28 13:13:16 -0600

    Version 16.1
    - Fixed lua error
    - Updated LibSavedVars to v4.0.1

--- Roomba.lua
+++ Roomba.lua
@@ -761,7 +761,7 @@ local function InitialiseSettings()
         name = ADDON_NAME,
         displayName = ZO_HIGHLIGHT_TEXT:Colorize(ADDON_NAME),
         author = "Wobin, CrazyDutchGuy, Ayantir & |c99CCEFsilvereyes|r",
-        version = "16",
+        version = "16.1",
         slashCommand = "/roomba",
         registerForRefresh = true,
         registerForDefaults = true,
